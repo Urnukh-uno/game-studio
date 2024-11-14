9f5537d83bac5c93ae80818e7f6adb8df77c15de
Urnukh <urnukh.uno@gmail.com>
2024-11-14 20:34:36 +0800

    aulgui baidliig huchtei bolgoson

--- Node.js
+++ Node.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const sql = require('mysql');
 const connection = sql.createConnection({
   host: 'localhost',
@@ -6,13 +8,30 @@ const connection = sql.createConnection({
   database: 'game_db'
 });
 
-connection.query('SELECT * FROM users WHERE id = ?', [userId], function(err, results) {
+// Нууц үгийг шифрлэх
+bcrypt.hash(userPassword, 10, (err, hashedPassword) => {
   if (err) throw err;
-  console.log(results);
+  
+  // Хэрэглэгчийг нэмэх
+  connection.query('INSERT INTO users (email, password) VALUES (?, ?)', [email, hashedPassword], function(err, results) {
+    if (err) throw err;
+    console.log('Шинэ хэрэглэгч амжилттай нэмэгдлээ');
+  });
 });
 
-const express = require('express');
-const helmet = require('helmet');
-const app = express();
-app.use(helmet()); // Таны вэбсайтын аюулгүй байдлыг хамгаална
-app.listen(3000, () => console.log('Server is running'));
+// Хэрэглэгчийн мэдээллийг шалгах
+connection.query('SELECT * FROM users WHERE email = ?', [email], function(err, results) {
+  if (err) throw err;
+
+  // Нууц үгийг зөвшөөрөх
+  bcrypt.compare(userPassword, results[0].password, (err, isMatch) => {
+    if (err) throw err;
+    if (isMatch) {
+      // JWT токен үүсгэх
+      const token = jwt.sign({ userId: results[0].id }, 'secretKey', { expiresIn: '1h' });
+      console.log('JWT токен:', token);
+    } else {
+      console.log('Нууц үг буруу');
+    }
+  });
+});
