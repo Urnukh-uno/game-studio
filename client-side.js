b0a9d00c5fdf4ccee18f8722cdef8d2e875fe9af
Urnukh <urnukh.uno@gmail.com>
2024-11-19 21:13:49 +0800

    contact hesgiig uurchilsun

--- client-side.js
+++
@@ -1,6 +0,0 @@
-if (window.location.protocol !== "https:") {
-    window.location.href = "https://" + window.location.host + window.location.pathname;
-} else {
-    // SSL сертификат баталгаажсан эсэхийг шалгах
-    console.log('Secure connection established');
-}
